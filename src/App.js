a939aaa0c278868f456df9ff114d0de14991f413
MD.Taahaa Bakhshi <taahaabakhshi7970@gamil.com>
2024-11-28 22:42:01 +0530

    added routes,api,filter Searching property

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import "./App.css";
 import Footer from "./components/footer/Footer";
-import Header  from "./components/header/Header";
+import Header from "./components/header/Header";
 import Hero from "./components/hero/Hero";
 import Product from "./components/product/Product";
 import Slider from "./components/slider/Slider";
 import Testimonials from "./components/testimonials/Testimonials";
 import Virtual from "./components/virtual/Virtual";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Brand from "./components/pages/brand";
+import Collections from "./components/pages/collections";
+import NewItem from "./components/pages/newItem";
+import { Sales } from "./components/pages/sales";
+import NotFound from "./components/pages/notFound";
+
 function App() {
   return (
-   <div>
-<Header/>
-<Hero/>
-<Slider/>
-<Virtual/>
-<Product/>
-<Testimonials/>
-<Footer/>
-   </div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Hero />
+                <Slider />
+                <Virtual />
+                <Product />
+                <Testimonials />
+                <Footer />
+              </>
+            }
+          />
+          <Route path="/brand" element={<Brand />} />
+          <Route path="/newItem" element={<NewItem />} />
+          <Route path="/sales" element={<Sales />} />
+          <Route path="/collections" element={<Collections />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,39 +1,58 @@
-import React, { useState } from 'react'
-import css from "./Header.module.css"
-import Logo from '../../assets/logo.png'
-import {CgShoppingBag} from 'react-icons/cg'
-import { VscThreeBars} from 'react-icons/vsc'
- const Header = () => {
-
-  const[ShowMenu,SetShowMenu]=useState(true)
-const TogleMenu =()=>{
-  SetShowMenu(!ShowMenu)
-}
-
+import React, { useState } from "react";
+import css from "./Header.module.css";
+import Logo from "../../assets/logo.png";
+import { CgShoppingBag } from "react-icons/cg";
+import { VscThreeBars } from "react-icons/vsc";
+import { useLocation } from "react-router-dom"
+import { Link } from "react-router-dom";
+const Header = () => {
+  const [ShowMenu, SetShowMenu] = useState(true);
+  const TogleMenu = () => {
+    SetShowMenu(!ShowMenu);
+  };
+  const location = useLocation();
   return (
     <div className={css.container}>
-      <div className={css.logo}>
-        <img src={Logo} alt="Logo"/>
+
+      <Link to="/" className={css.logo} >
+        <img src={Logo} alt="Logo" />
         <span>amazon</span>
-      </div>
+      </Link>
       <div className={css.right}>
-
         <div className={css.bars} onClick={TogleMenu}>
-          < VscThreeBars/>
+          <VscThreeBars />
         </div>
-      
-          <ul className={css.menu} style={{display:ShowMenu? 'inherit':'none'}}>
-         <li>Collections</li>
-         <li>brands</li>
-         <li>new</li>
-         <li>sales</li>
-         <li>ENG</li>
-          </ul>
 
-        <input type="text" className={css.search} placeholder='Search....' />
-<CgShoppingBag className={css.cart}/>
+        <ul
+          className={css.menu}
+          style={{ display: ShowMenu ? "inherit" : "none" }}
+        >
+          <li>
+            <Link to="/collections"> Collections</Link>
+          </li>
+          <li>
+            <Link to="/brand">brands</Link>
+          </li>
+          <li>
+            <Link to="/newItem">new</Link>
+          </li>
+          <li>
+            <Link to="/sales">sales</Link>
+          </li>
+          <li>ENG</li>
+        </ul>
+
+
+      
+        {location.pathname === "/collections" && (
+          <input type="text" className={css.search} placeholder="Search...." />
+        )}
+
+
+        {/* <input type="text" className={css.search} placeholder="Search...." /> */}
+        <CgShoppingBag className={css.cart} />
       </div>
     </div>
-  )
-}
+  );
+};
 export default Header;
